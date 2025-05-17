88a876f82515dd4bf965ff1022f0e13407198d1a
MosaabSaleem <mmosaab2002@hotmail.com>
2025-05-17 21:11:42 +1000

    FInished some navbar styling

--- mry_portfolio/src/Components/NavBar.jsx
+++ mry_portfolio/src/Components/NavBar.jsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import "../Styles/NavBar.css";
+import MRYLogo from "../Assets/MRYLogoNavbar.png";
 
 export default function NavBar() {
+
+    const handleContactClick = () => {
+        // Logic to handle contact button click
+        // For example, you can navigate to the contact page or open a modal
+        console.log("Contact button clicked");
+    }
+    const handleAboutClick = () => {
+        // Logic to handle about button click
+        console.log("About button clicked");
+    }
+
+    const handleProjectsClick = () => {
+        // Logic to handle projects button click
+        console.log("Projects button clicked");
+    }
+
     return (
         <nav className="navbar">
-            <div className="navbar__logo">
-                <Link to="/">My Portfolio</Link>
+            <div className="navbarlogo">
+                <Link to="/">
+                    <img className="navbarlogoimage" src={MRYLogo}></img>
+                </Link>
             </div>
             <div className="navlinks">
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/about" className="navlink">About us</Link>
+                <Link to="/projects" className="navlink">Projects</Link>
+                <Link to="/contact" className="navlink">Contact</Link>
             </div>
         </nav>
     );
