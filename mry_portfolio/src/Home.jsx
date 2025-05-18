33a847972306c993799de7aeacd1a786dbe37b6f
MosaabSaleem <mmosaab2002@hotmail.com>
2025-05-18 21:50:01 +1000

    Changed the design entirely. focsuing on a more modular design

--- mry_portfolio/src/Home.jsx
+++ mry_portfolio/src/Home.jsx
@@ -3,11 +3,11 @@ import "./Styles/Home.css";
 function Home() {
   return (
     <div className="home">
-      <h1 className="homeTitle">Turning Your <span className="titleHighlight">Ideas </span>Into Reality</h1>
-      <div className="homeTextContainer">
-        <p className="homeText">Explore some of our projects or contact us now!</p>
+      <div className="homeTitleContainer">
+        <h1 className="homeTitle">Turning Your <span className="titleHighlight">Ideas </span>Into Reality</h1>
         <button className="homeEnquireButton">Enquire Now</button>
       </div>
+      <p className="homeText">Explore some of our projects or contact us now!</p>
     </div>
   );
 }
