c550493e8ecd644896523d408133ee5e51565a3e
MosaabSaleem <mmosaab2002@hotmail.com>
2025-05-18 21:06:23 +1000

    Finished a darkmode draft of home page

--- mry_portfolio/src/Components/NavBar.jsx
+++ mry_portfolio/src/Components/NavBar.jsx
@@ -1,36 +1,19 @@
 import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
 import "../Styles/NavBar.css";
 import MRYLogo from "../Assets/MRYLogoNavbar.png";
 
 export default function NavBar() {
-
-    const handleContactClick = () => {
-        // Logic to handle contact button click
-        // For example, you can navigate to the contact page or open a modal
-        console.log("Contact button clicked");
-    }
-    const handleAboutClick = () => {
-        // Logic to handle about button click
-        console.log("About button clicked");
-    }
-
-    const handleProjectsClick = () => {
-        // Logic to handle projects button click
-        console.log("Projects button clicked");
-    }
-
     return (
         <nav className="navbar">
             <div className="navbarlogo">
                 <Link to="/">
-                    <img className="navbarlogoimage" src={MRYLogo}></img>
+                    <img className="navbarlogoimage" src={MRYLogo} alt="MRY Logo"></img>
                 </Link>
             </div>
             <div className="navlinks">
-                <Link to="/about" className="navlink">About us</Link>
-                <Link to="/projects" className="navlink">Projects</Link>
                 <Link to="/contact" className="navlink">Contact</Link>
+                <Link to="/projects" className="navlink">Projects</Link>
+                <Link to="/about" className="navlink">About us</Link>
             </div>
         </nav>
     );
--- mry_portfolio/src/Home.jsx
+++ mry_portfolio/src/Home.jsx
@@ -5,8 +5,8 @@ function Home() {
     <div className="home">
       <h1 className="homeTitle">Turning Your <span className="titleHighlight">Ideas </span>Into Reality</h1>
       <div className="homeTextContainer">
-        <p className="homeText">Feel free to explore some of our projects.</p>
-        <p className="homeText">or <a href="http">click here to contact us and start building!</a></p>
+        <p className="homeText">Explore some of our projects or contact us now!</p>
+        <button className="homeEnquireButton">Enquire Now</button>
       </div>
     </div>
   );
