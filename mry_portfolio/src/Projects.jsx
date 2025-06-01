cfa6492a0e4c7c694ffa34687da506bd54c655bb
MosaabSaleem <mmosaab2002@hotmail.com>
2025-06-01 21:42:35 +1000

    Projects formatting is done. Just need to collect content

--- mry_portfolio/src/Projects.jsx
+++ mry_portfolio/src/Projects.jsx
@@ -1,23 +1,53 @@
 import "./Styles/Projects.css";
+import Cookies from "./Assets/CookiesExample.png";
 
 export default function Projects() {
     return (
         <div className="Projects">
+          {/* <div className="ProjectTitleContainer"> */}
             <h1 className="ProjectsTitle">Our Projects</h1>
-            <p className="ProjectsText">Explore some of our projects below!</p>
-            <div className="ProjectsList">
-                <div className="ProjectCard">
-                    <h2 className="ProjectTitle">Project 1</h2>
-                    <p className="ProjectDescription">Description of Project 1.</p>
+            <p className="ProjectsSubTitle">Explore some of our projects below!</p>
+          {/* </div> */}
+            
+          <div className="ProjectsList">
+            <a href="https://wonderful-dune-016b4c61e.6.azurestaticapps.net/" target="_blank" rel="noopener noreferrer">
+              <div className="ProjectCard">
+                <div className="ProjectCardleft">
+                  <img src={Cookies} alt="Project 1" className="ProjectCardImage" />
                 </div>
-                <div className="ProjectCard">
-                    <h2 className="ProjectTitle">Project 2</h2>
-                    <p className="ProjectDescription">Description of Project 2.</p>
+                <div className="ProjectCardRight">
+                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
+                  <p className="ProjectDescription">Description of Project 1.</p>
                 </div>
-                <div className="ProjectCard">
-                    <h2 className="ProjectTitle">Project 3</h2>
-                    <p className="ProjectDescription">Description of Project 3.</p>
+              </div>
+            </a>
+              <div className="ProjectCard">
+                <div className="ProjectCardleft">
+                  <img src={Cookies} alt="Project 2" className="ProjectCardImage" />
                 </div>
+                <div className="ProjectCardRight">
+                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
+                  <p className="ProjectDescription">Description of Project 2.</p>
+                </div>
+              </div>
+              <div className="ProjectCard">
+                <div className="ProjectCardleft">
+                  <img src={Cookies} alt="Project 3" className="ProjectCardImage" />
+                </div>
+                <div className="ProjectCardRight">
+                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
+                  <p className="ProjectDescription">Description of Project 3.</p>
+                </div>
+              </div>
+              <div className="ProjectCard">
+                <div className="ProjectCardleft">
+                  <img src={Cookies} alt="Project 4" className="ProjectCardImage" />
+                </div>
+                <div className="ProjectCardRight">
+                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
+                  <p className="ProjectDescription">Description of Project 4.</p>
+                </div>
+              </div>
             </div>
         </div>
     );
