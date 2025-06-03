61cb5765cd8b56d7baf06c59fda29704825b4b8c
MosaabSaleem <mmosaab2002@hotmail.com>
2025-06-03 21:56:50 +1000

    Added some more content to projects
    Still need to add some more real projects

--- mry_portfolio/src/Projects.jsx
+++ mry_portfolio/src/Projects.jsx
@@ -1,5 +1,6 @@
 import "./Styles/Projects.css";
 import Cookies from "./Assets/CookiesExample.png";
+import ComingSoon from "./Assets/ComingSoon.png";
 
 export default function Projects() {
     return (
@@ -17,37 +18,39 @@ export default function Projects() {
                 </div>
                 <div className="ProjectCardRight">
                   <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
-                  <p className="ProjectDescription">Description of Project 1.</p>
+                  <p className="ProjectDescription">Online shop for a local cookie businesses. Straight to the point, it allows for an easy customer experience and accepts payments for you!</p>
                 </div>
               </div>
             </a>
               <div className="ProjectCard">
                 <div className="ProjectCardleft">
-                  <img src={Cookies} alt="Project 2" className="ProjectCardImage" />
+                  <img src={ComingSoon} alt="Project 2" className="ProjectCardImage" />
                 </div>
                 <div className="ProjectCardRight">
-                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
+                  <h2 className="ProjectCardTitle">Project 2</h2>
                   <p className="ProjectDescription">Description of Project 2.</p>
                 </div>
               </div>
               <div className="ProjectCard">
                 <div className="ProjectCardleft">
-                  <img src={Cookies} alt="Project 3" className="ProjectCardImage" />
+                  <img src={ComingSoon} alt="Project 3" className="ProjectCardImage" />
                 </div>
                 <div className="ProjectCardRight">
-                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
+                  <h2 className="ProjectCardTitle">Project 3</h2>
                   <p className="ProjectDescription">Description of Project 3.</p>
                 </div>
               </div>
+              <a href="https://github.com/MosaabSaleem?tab=repositories" target="_blank" rel="noopener noreferrer">
               <div className="ProjectCard">
                 <div className="ProjectCardleft">
-                  <img src={Cookies} alt="Project 4" className="ProjectCardImage" />
+                  <img src={ComingSoon} alt="Project 4" className="ProjectCardImage" />
                 </div>
                 <div className="ProjectCardRight">
-                  <h2 className="ProjectCardTitle">Local Cookie Shop</h2>
-                  <p className="ProjectDescription">Description of Project 4.</p>
+                  <h2 className="ProjectCardTitle">Check Upcoming Projects</h2>
+                  <p className="ProjectDescription">We're always working on projects to display and improve or skills. Stay on the lookout and check us out on Github!</p>
                 </div>
               </div>
+              </a>
             </div>
         </div>
     );
